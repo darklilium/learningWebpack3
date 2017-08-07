0c32f421aa235294cc7c71a3f756fd28e6ed35ac
Evelyn Hernández <darklilium@users.noreply.github.com>
2017-08-07 17:25:21 -0400

    Preparando para guia de Output Management

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import _ from 'lodash';
-import './index.css';
-import Icon from './chq.png';
-import Data from './data.xml';
+
 
 function component() {
   var element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
+
  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
- element.classList.add('hello');
- // Add the image to our existing div.
- var myIcon = new Image();
- myIcon.src = Icon;
 
- element.appendChild(myIcon);
 
- console.log(Data);
- 
+
   return element;
 }
 
